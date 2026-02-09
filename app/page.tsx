cd92ad271d33c45260c055b0636998dc0ceb2a9e
neragit <nerazd@hotmail.com>
2026-02-10 00:40:43 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -192,10 +192,10 @@ export default function Home() {
 
 
         <main
-          className={`flex-1 min-w-0  text-gray-700 
+          className={`flex-1  min-w-0  text-gray-700 
             transition-all duration-100
             ${sidebarVisible ? "pl-60" : "pl-5"}
-            portrait:pl-10  lg:pl-52
+            portrait:px-7  lg:pl-52
             max-w-full `}
           id="start"
         >
